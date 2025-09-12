7e42393214fdc616095fb84d154e5e1b75e01270
Miannaise <39148291+Miannaise@users.noreply.github.com>
2025-09-12 08:51:05 -0600

    Added Covariance

--- pythonsrc/feature_analysis.py
+++ pythonsrc/feature_analysis.py
@@ -3,5 +3,23 @@ import numpy as np
 from train import X_train, Y_non_violent, Y_violent
 import typing
 
-for col in X_train:
-    print(np.cov(X_train[col],Y_violent))
+#We start with covariance since it is trivial to analyze
+def covariance(x, y):
+    x = np.asarray(x)
+    y = np.asarray(y)
+    if x.shape != y.shape:
+        raise ValueError(f"Shapes do not match x.shape:{x.shape}, y.shape:{y.shape}")
+    return np.mean((x - x.mean()) * (y - y.mean()))
+
+#Read columns from columns.txt for easy callback.
+with open("./proj1files/columns.txt",'r') as f:
+    f.read()
+
+#Avoid h(x)=1, start from real features
+cov_arr=[]
+for col in range(1,78):
+    cov_arr.append(covariance(X_train[:,col],Y_violent))
+
+top_five=cov_arr[-5:]
+for i in top_five:
+    print(i)
--- pythonsrc/train.py
+++ pythonsrc/train.py
@@ -1,39 +1,39 @@
 import scipy.io as scio
 import numpy as np
 #Load the matlab file - Mia
-training_data=scio.loadmat('../proj1files/trainingdata.mat')
+training_data=scio.loadmat("./proj1files/trainingdata.mat")
 #Weird Dict, one key being "trainingdata" and the value is an np.ndarray
 #Extract that out of that
 training_data=training_data["trainingdata"]
-print(type(training_data))
+#print(type(training_data))
 #X data
 X_train=training_data[:,0:78]
-print(np.shape(X_train))
+#print(np.shape(X_train))
 #violent crimes.
 Y_violent=training_data[:,79]
-print(np.shape(Y_violent))
+#print(np.shape(Y_violent))
 #non violent crime
 Y_non_violent=training_data[:,80]
 #Insert Ones into X_train for simple linear regression
 X_train = np.insert(X_train, 0, np.ones((X_train.shape[0],)), axis=1)
-print(np.shape(X_train))
-print(X_train)
+#print(np.shape(X_train))
+#print(X_train)
 
 
 #Least Squares Starts Here
 betaviolent, *_ = np.linalg.lstsq(X_train, Y_violent, rcond=None)      # shape (80,)
 betanonviolent, *_ = np.linalg.lstsq(X_train, Y_non_violent, rcond=None) # shape (80,)
-print(len(betaviolent))
-print(len(betanonviolent))
+#print(len(betaviolent))
+#print(len(betanonviolent))
 
 #Evaluate model performance
 #Recall MAPE is mean absolute percentage error 
 # Violent crime MAPE
 y_pred_violent = X_train @ betaviolent
 MAPE_violent = 100 * np.sum(np.abs(y_pred_violent - Y_violent) / Y_violent) / len(Y_violent)
-print(f"MAPE for the violent crime rate training set is {MAPE_violent:.1f}")
+#print(f"MAPE for the violent crime rate training set is {MAPE_violent:.1f}")
 
 # Nonviolent crime MAPE
 y_pred_nonviolent = X_train @ betanonviolent
 MAPE_nonviolent = 100 * np.sum(np.abs(y_pred_nonviolent - Y_non_violent) / Y_non_violent) / len(Y_non_violent)
-print(f"MAPE for the nonviolent crime rate training set is {MAPE_nonviolent:.1f}")
+#print(f"MAPE for the nonviolent crime rate training set is {MAPE_nonviolent:.1f}")
